12eafc5c999826a22c4198f11ff464fe8312ac80
gagno080 <36488545+gagno080@users.noreply.github.com>
2022-06-06 22:35:17 -0700

    code update as of 6.6.22

--- src/components/Input/DisplayJSON.js
+++ src/components/Input/DisplayJSON.js
@@ -5,11 +5,28 @@ import classes from "./DisplayJSON.module.css";
 const DisplayJSON = () => {
   const [files, setFiles] = useState("");
 
+  /*
   const handleChange = (e) => {
     const fileReader = new FileReader();
     fileReader.readAsText(e.target.files[0], "UTF-8");
     fileReader.onload = (e) => {
       console.log("e.target.result", e.target.result);
+      setFiles(e.target.result);
+      var myObject = JSON.parse("e.target.result");
+      console.log(myObject.fruit);
+    };
+  };
+  */
+
+  const handleChange = (e) => {
+    //console.log("Please do something");
+    const fileReader = new FileReader();
+    fileReader.readAsText(e.target.files[0], "UTF-8");
+    var thing = fileReader.result;
+    console.log(thing);
+    fileReader.onload = (e) => {
+      console.log("e.target.result", e.target.result[0], e.target.result[1]);
+      console.log(e);
       setFiles(e.target.result);
     };
   };
@@ -20,6 +37,9 @@ const DisplayJSON = () => {
         <label>Upload Json file</label>
         <br />
         <br />
+        <input type="file" onChange={handleChange}/>
+        {files}
+        {/*
         <input type="file" onChange={handleChange} />
         <br />
         <br />
@@ -29,6 +49,7 @@ const DisplayJSON = () => {
         {files}
         <br />
         <br />
+        */}
     </Card>
   );
 };
